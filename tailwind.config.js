686872b50ade10418e487c58a02b620627c0fa05
Contravo Admin <admin@contravo.com>
2025-06-25 21:38:54 +0300

    fix(config): convert tailwind and postcss configs to ES modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -80,6 +82,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
 
